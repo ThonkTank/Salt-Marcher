a3733d6cf892cfcdbfb97ce72f403990e6f06de7
ThonkTank <129946818+ThonkTank@users.noreply.github.com>
2025-09-23 12:19:18 +0200

    Remove hex map aggregator and update overviews

--- src/core/hex-mapper/hex-map.ts
+++
@@ -1,4 +0,0 @@
-// src/core/hex-mapper/hex-map.ts
-export { parseOptions } from "../options";
-export { renderHexMap } from "./hex-render";
-export { getAllMapFiles, getFirstHexBlock } from "../map-list";
