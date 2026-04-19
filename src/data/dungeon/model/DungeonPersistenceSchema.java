7773923f6331473db13a2d5a798028894665fb9f
Aaron <aaron.wuester@outlook.de>
2026-04-20 00:15:56 +0200

    wip(quality): preserve current state before quality checks

--- src/data/dungeon/model/DungeonPersistenceSchema.java
+++ src/data/dungeon/model/DungeonPersistenceSchema.java
@@ -2,6 +2,8 @@ package src.data.dungeon.model;
 
 public final class DungeonPersistenceSchema {
 
+    public static final String DATABASE_FILE_NAME = "game.db";
+
     private DungeonPersistenceSchema() {
     }
 }
